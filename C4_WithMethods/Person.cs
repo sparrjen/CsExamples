48926014fff8b85a3d4c0ff4688603429563d77d
Jennie Sparr <jennie.sparr@learnet.se>
2020-09-02 20:49:08 +0200

    C4

---
+++ C4_WithMethods/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace C4_WithMethods
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+
+        public void AskForName()
+        {
+            Console.Write("Welcome! What's your name? ");
+            Name = Console.ReadLine();
+
+            Console.Write($"Nice, {Name}. ");
+        }
+        public void AskForAge()
+        {
+            Console.Write("How old are you? ");
+            try
+            {
+                Age = Convert.ToInt32(Console.ReadLine());
+            }
+
+            catch
+            {
+                Console.WriteLine("Sorry, your age must be in digits. Try again please!");
+                Console.Write("How old are you? ");
+
+                try
+                {
+                    Age = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Wow, you are not the brightest tool in the shed!");
+                }
+
+            }
+            finally
+            {
+                Console.WriteLine($"Ok, awesome! I am the same age. I am also {Age}.");
+            }
+
+          
+
+        }
+        public void NameContains()
+        {
+            Console.Write($"Hey, your name contain {Name.Length} charachters. ");
+        }
+        public void AskForCity()
+        {
+            Console.Write($"{Name} where do you live? ");
+            City = Console.ReadLine();
+            Console.WriteLine($"That's cool. My sister lives in {City}.");
+        }
+    }
+}
---
+++ C4_WithMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C4_WithMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            person.AskForName();
+            person.AskForAge();
+            person.AskForCity();
+
+        }
+    }
+}
