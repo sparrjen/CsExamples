2030d0ed2e1fe7a96ca2605f3b559c586ba6cc74
Jennie Sparr <jennie.sparr@learnet.se>
2020-09-02 15:25:22 +0200

    C3

---
+++ C3_WithAClass/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C3_WithAClass
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+        public Person() //Konstruktor. När du skapar en klass som du använder finns per auto en konstruktor som deklarerar, ger värde 0 på ovan int vid användade. Behöver ej skrivas ut!
+        {
+
+        } 
+
+    }
+
+ 
+    
+
+}
---
+++ C3_WithAClass/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace C3_WithAClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            Console.Write("Welcome! What's your name? ");
+            person.Name = Console.ReadLine();
+
+            Console.Write($"Nice {person.Name}. How old are you? ");
+            //age = Convert.ToInt32(Console.ReadLine());
+            // alternativt:   age = int.Parse(Concole.ReadLine());   Detta går att använda för alla datatyper, ex double, då double.Parse(Concole.ReadLine());
+
+            try
+            {
+                person.Age = Convert.ToInt32(Console.ReadLine());
+            }
+
+            catch
+            {
+                Console.WriteLine("Sorry, your age must be in digits. Try again please!");
+                Console.Write("How old are you? ");
+
+                try
+                {
+                    person.Age = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Wow, you are not the brightest tool in the shed!");
+                }
+
+            }
+            finally
+            {
+                Console.WriteLine($"Ok, awesome! I am the same age. I am also {person.Age}.");
+            }
+
+            Console.Write($"Hey, your name contain {person.Name.Length} charachters. ");
+            Console.Write($"{person.Name} where do you live? ");
+            person.City = Console.ReadLine();
+            Console.WriteLine($"That's cool. My sister lives in {person.City}.");
+
+        }
+    }
+}
