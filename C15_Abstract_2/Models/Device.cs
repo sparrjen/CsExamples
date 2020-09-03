d7eb268eaf0e73ded1461faa105cbb55a55a8e31
Jennie Sparr <jennie.sparr@learnet.se>
2020-09-03 14:56:23 +0200

    C15

---
+++ C15_Abstract_2/Models/Device.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C15_Abstract_2.Models
+{
+    abstract class Device
+    {
+        public string CreateFromConnectionString(string connectionstring)
+        {
+            return $"initializing connection with\\))"; 
+        }
+    }
+}
