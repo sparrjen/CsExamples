b6b23bfe368ed7052587dfc2ccefe4917599e084
Jennie Sparr <jennie.sparr@learnet.se>
2020-09-03 12:30:59 +0200

    C11

--- C11_Enum/Program.cs
+++ C11_Enum/Program.cs
@@ -30,12 +30,12 @@ namespace C11_Enum
 
             switch(temperature)
             {
-                case int t when t < 10:
+                case var t when t < 10:
                     _tempLevel = Level.Low;
                     Console.WriteLine("Temperature is to Low");
                     break;
 
-                case int t when t > 30:
+                case var t when t > 30:
                     _tempLevel = Level.High;
                     Console.WriteLine("Temperature is to High");
                     break;
@@ -49,15 +49,40 @@ namespace C11_Enum
 
             switch(_tempLevel)
             {
-               
+                case Level.Low:
+                    Console.WriteLine("Temperature is to Low");
+                    break;
 
+                case Level.Normal:
+                    Console.WriteLine("Temperature is Normal");
+                    break;
 
-
+                case Level.High:
+                    Console.WriteLine("Temperature is to High");
+                    break;
 
             }
-            
-                
-         
+
+            var status = Status.Transporting;
+
+            switch (status)
+            {
+                case Status.Pending:
+                    Console.WriteLine("Electronic information has been received.");
+                    break;
+                case Status.Production:
+                    Console.WriteLine("Your product(s) are beeing prepared for shipping.");
+                    break;
+                case Status.Transporting:
+                    Console.WriteLine("Your product(s) are in transit.");
+                    break;
+                case Status.Delivered:
+                    Console.WriteLine("Your product(s) has been delivered.");
+                    break;
+            }
+
+
+
         }
     }
 }
