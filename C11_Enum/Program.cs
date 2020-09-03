c7b693fbe15f55b1fc39e28f92725b3e95e832b2
Jennie Sparr <jennie.sparr@learnet.se>
2020-09-03 10:59:47 +0200

    C11

---
+++ C11_Enum/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace C11_Enum
+{
+    public enum Level
+    {
+        Low,
+        Normal,
+        High
+
+    }
+
+
+    class Program
+    {
+
+        enum Status
+        {
+           Pending,
+           Production,
+           Transporting,
+           Delivered
+        }
+
+
+        static void Main(string[] args)
+        {
+            var temperature = 32;
+            var _tempLevel = Level.Normal;
+
+            switch(temperature)
+            {
+                case int t when t < 10:
+                    _tempLevel = Level.Low;
+                    Console.WriteLine("Temperature is to Low");
+                    break;
+
+                case int t when t > 30:
+                    _tempLevel = Level.High;
+                    Console.WriteLine("Temperature is to High");
+                    break;
+
+                default:
+                    _tempLevel = Level.Normal;
+                    Console.WriteLine("Temperature is Normal");
+                    break;
+
+            }
+
+            switch(_tempLevel)
+            {
+               
+
+
+
+
+            }
+            
+                
+         
+        }
+    }
+}
+
+
+//Enum kan skrivas i namespace eller i class.
