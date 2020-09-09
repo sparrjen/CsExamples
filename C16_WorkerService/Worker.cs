7ef20161838ee00d1f1361dcf4e9b63031decdc2
Jennie Sparr <jennie.sparr@learnet.se>
2020-09-09 23:31:24 +0200

    C16

--- C16_WorkerService/Worker.cs
+++ C16_WorkerService/Worker.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
@@ -10,20 +12,63 @@ namespace C16_WorkerService
 {
     public class Worker : BackgroundService
     {
+
+
         private readonly ILogger<Worker> _logger;
+        private readonly string _url = "https://google.com";
+
+        private HttpClient _client;
+        private HttpResponseMessage _result;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
         }
 
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _client = new HttpClient();
+            _logger.LogInformation("The service has been started.");
+
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _client.Dispose();
+            _logger.LogInformation("The service has been stopped.");
+            return base.StopAsync(cancellationToken);
+        }
+
+
+
+
+
+
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                try
+                {
+                    _result = await _client.GetAsync(_url);
+
+                    if (_result.IsSuccessStatusCode)
+                        _logger.LogInformation($"The website ({_url}) is up. Status Code = {_result.StatusCode}");
+                    else
+                        _logger.LogInformation($"The website ({_url}) is down. Status Code = {_result.StatusCode}");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogInformation($"Failed. The website ({_url}) - {ex.Message}");
+                }
+
+
+                await Task.Delay(10 * 1000, stoppingToken);
             }
         }
     }
 }
+
---
+++ C16_WorkerService/_result/IsSuccessStatusCode.cs
@@ -0,0 +1,6 @@
+﻿namespace _result
+{
+    internal class IsSuccessStatusCode
+    {
+    }
+}
