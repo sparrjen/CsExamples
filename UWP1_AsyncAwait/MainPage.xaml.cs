d592f00c243612bdfd1a8e8a34adfedd743d31f7
Jennie Sparr <jennie.sparr@learnet.se>
2020-09-10 14:33:03 +0200

    UWP1

--- UWP1_AsyncAwait/MainPage.xaml.cs
+++ UWP1_AsyncAwait/MainPage.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +29,59 @@ namespace UWP1_AsyncAwait
         public MainPage()
         {
             this.InitializeComponent();
+
+
         }
+
+        private void SyncButton_Click(object sender, RoutedEventArgs e)
+        {
+            ExecutionResult.Text = "";
+            TimeResult.Text = "";
+            var watch = Stopwatch.StartNew();
+
+            RunExample(10);
+
+            watch.Stop();
+            TimeResult.Text = watch.ElapsedMilliseconds.ToString();
+        }
+
+        private async void ASyncButton_Click(object sender, RoutedEventArgs e)
+        {
+            ExecutionResult.Text = "";
+            TimeResult.Text = "";
+            var watch = Stopwatch.StartNew();
+
+            await RunExampleAsync(10);
+
+            watch.Stop();
+            TimeResult.Text = watch.ElapsedMilliseconds.ToString();
+        }
+
+
+
+        private void RunExample(int seconds)
+        {
+            for (var i = 0; i < seconds; i++)
+                ExecutionResult.Text += DoWork(i);
+
+            ExecutionResult.Text += "Completed";
+        }
+
+        private async Task RunExampleAsync(int seconds)
+        {
+            for (var i = 0; i < seconds; i++)
+                ExecutionResult.Text += await Task.Run(() => DoWork(i));
+
+            ExecutionResult.Text += "Completed";
+        }
+
+
+
+        private string DoWork(int i)
+        {
+            Thread.Sleep(1000);
+            return $"loop #{i + 1} {Environment.NewLine}";
+        }
+
     }
 }
