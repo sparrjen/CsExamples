b0b0082f275a3b306ae991c682a6b8ae98d1d1c7
Jennie Sparr <jennie.sparr@learnet.se>
2020-09-11 10:16:37 +0200

    C16

--- C16_WorkerService/Program.cs
+++ C16_WorkerService/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace C16_WorkerService
 {
@@ -11,14 +13,38 @@ namespace C16_WorkerService
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.File(@"c:\workerservice\log\LogFile.txt")
+            .CreateLogger();
 
+
+            try
+            {
+                Log.Information("Starting WorkerService...");
+                CreateHostBuilder(args).Build().Run();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, $"WorkerService terminated unexpectedly. Error:: {ex.Message}");
+                return;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+        }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<Worker>();
-                });
+           Host.CreateDefaultBuilder(args)
+               .UseSerilog()
+               .UseWindowsService()
+               .ConfigureServices((hostContext, services) =>
+               {
+                   services.AddHostedService<Worker>();
+               });
     }
 }
