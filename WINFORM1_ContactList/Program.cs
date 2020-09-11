136f420e81c1af3be97d97066e2a9205306249fb
Jennie Sparr <jennie.sparr@learnet.se>
2020-09-11 12:31:58 +0200

    WPF1

--- WINFORM1_ContactList/Program.cs
+++ WINFORM1_ContactList/Program.cs
@@ -18,6 +18,8 @@ namespace WINFORM1_ContactList
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //använd EJ, använd UWP eller WPF
         }
     }
 }
